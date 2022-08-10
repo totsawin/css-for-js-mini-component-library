1e89594d2a8f520938bd1dc0f057a3a368bae16d
totsawin <i_amnothere@live.com>
2022-08-10 14:42:36 +0700

    implement progress bar with styled component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,54 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let style = {
+    'small': {
+      '--height': '8px',
+      '--padding': '0px',
+      '--radius': '4px'
+    },
+    'medium': {
+      '--height': '12px',
+      '--padding': '0px',
+      '--radius': '4px'
+    },
+    'large': {
+      '--height': '24px',
+      '--padding': '4px',
+      '--radius': '8px'
+    }
+  }
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={style[size]}>
+      <BarWrapper>
+        <Bar value={value > 100 ? 100: value}></Bar>
+      </BarWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background: ${COLORS.transparentGray15};
+  height: var(--height);
+  padding: var(--padding);
+  border-radius: var(--radius);
+`;
+
+const BarWrapper = styled.div`
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: ${p => p.value}%;
+  height: 100%;
+  background: ${COLORS.primary};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  border-top-right-radius: ${p => p.value === 100 ? '4px': '0px'};
+  border-bottom-right-radius: ${p => p.value === 100 ? '4px': '0px'};
+`;
 
 export default ProgressBar;
