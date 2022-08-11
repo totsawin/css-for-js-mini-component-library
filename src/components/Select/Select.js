9cd3e7273a1bb75100046eb9f57992928d6a0df7
totsawin <i_amnothere@live.com>
2022-08-11 20:02:53 +0700

    implement select input

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,12 +7,55 @@ import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-
+  console.log(displayedValue);
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
+        { children }
+      </NativeSelect>
+      <PresentationSelect>
+        { displayedValue }
+        <Icon id="chevron-down" strokeWidth="2" size="16"/>
+      </PresentationSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: fit-content;
+  position: relative;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  opacity: 0;
+`;
+
+const PresentationSelect = styled.div`
+  background-color: ${ COLORS.transparentGray15 };
+  color: ${ COLORS.gray700 };
+  padding: 12px 16px;
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  line-height: 18.75px;
+  border-radius: 8px;
+  ${NativeSelect}:focus + & {
+    outline: 2px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  };
+  ${NativeSelect}:hover + & {
+    color: ${ COLORS.black };
+  }
+`;
 
 export default Select;
